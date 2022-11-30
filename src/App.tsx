f20c396e6bf137642f30c5a0c5f1917d1350ea48
Gyeongho Park <59360980+kayheypark@users.noreply.github.com>
2022-12-01 02:53:45 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const xxx = () => {
+    return
+    true
+}
+
+const App = () => {
+    return <h1>1</h1>
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+    const config = {
+        entry: path.join(__dirname, 'src', 'index.tsx'),
+        mode: options.mode,
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.[hash].js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.?(js|jsx|ts|tsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react'],
+                        },
+                    },
+                    resolve: {
+                        extensions: ['', '.js', '.jsx', '.ts', '.tsx'],
+                    },
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(png|jp(e*)g|svg|gif)$/,
+                    use: ['file-loader'],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, 'public', 'index.html'),
+            }),
+            new webpack.HotModuleReplacementPlugin(),
+        ],
+        devServer: {
+            hot: true,
+            host: 'localhost',
+            port: 3000,
+            open: true,
+        },
+    };
+    return config;
+};
