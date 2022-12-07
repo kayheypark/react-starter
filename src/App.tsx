a1ad0edc7d70e5eb06a112572c429c3c489d1c38
Gyeongho Park <59360980+kayheypark@users.noreply.github.com>
2022-12-08 01:45:42 +0900

    code clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import React from 'react';
-
-const xxx = () => {
-    return;
-    true;
-};
-
 const App = () => {
-    return <h1>1</h1>;
+    return <h1>Hello Wolrd</h1>;
 };
 
 export default App;
